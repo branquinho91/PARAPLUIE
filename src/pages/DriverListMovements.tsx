d698b08793b2d9cb1e0f29b77fc10b64f5ac53a0
Gustavo Branquinho <gustavobranquinho2@gmail.com>
2024-10-28 17:50:25 -0300

    Refactor DriverListMovements component to include UserHeader and update styling

--- src/pages/DriverListMovements.tsx
+++ src/pages/DriverListMovements.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text, FlatList, Alert } from "react-native";
+import UserHeader from "../components/UserHeader";
+import { useCallback, useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import axios from "axios";
 
-const DriverListMovements = () => {
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+const DriverListMovements = ({ navigation }: any) => {
   return (
-    <View>
-      <Text>DriverListMovements</Text>
+    <View style={styles.container}>
+      <UserHeader navigation={navigation} />
+
+      <Text style={styles.header}>Lista de Movimentações</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+});
 
 export default DriverListMovements;
